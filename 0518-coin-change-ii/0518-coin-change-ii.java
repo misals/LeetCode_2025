a90a5e1ca6016b61429a2aef2c59350d2c66d660
Suraj Misal <35091245+misals@users.noreply.github.com>
2025-07-13 18:24:22 +0530

    Time: 8 ms (68.74%), Space: 49.4 MB (30.84%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,33 +1,29 @@
 class Solution {
-    public int change(int ind, int tar, int[] coins, int[][] dp) {
-        if (ind == 0) {
-            if (tar % coins[0] == 0) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
-
-        if (dp[ind][tar] != -1) return dp[ind][tar];
-
-        int notPick = change(ind - 1, tar, coins, dp);
-        int pick = 0;
-        if (tar >= coins[ind]) {
-            pick = change(ind, tar - coins[ind], coins, dp);
-        }
-
-        return dp[ind][tar] = pick + notPick;
-    }
-
     public int change(int amount, int[] coins) {
         int n = coins.length;
 
         int[][] dp = new int[n][amount + 1];
 
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        for (int tar = 0; tar <= amount; tar++) {
+            if (tar % coins[0] == 0) {
+                dp[0][tar] = 1;
+            } else {
+                dp[0][tar] = 0;
+            }
         }
 
-        return change(n - 1, amount, coins, dp);
+        for (int ind = 1; ind < n; ind++) {
+            for (int tar = 0; tar <= amount; tar++) {
+                int notPick = dp[ind - 1][tar];
+                int pick = 0;
+                if (tar >= coins[ind]) {
+                    pick = dp[ind][tar - coins[ind]];
+                }
+
+                dp[ind][tar] = pick + notPick;
+            }
+        }
+
+        return dp[n - 1][amount];
     }
 }

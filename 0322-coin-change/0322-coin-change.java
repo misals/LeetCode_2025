6ca4a040f4a3b5fb2a416f844df091ac9fbd8515
Suraj Misal <35091245+misals@users.noreply.github.com>
2025-07-13 14:50:32 +0530

    Time: 15 ms (77.75%), Space: 45.6 MB (14.98%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,36 +1,27 @@
 class Solution {
-    public int coinChange(int ind, int amount, int[] coins, int[][] dp) {
-        if (ind == 0) {
-            if (amount % coins[ind] == 0) {
-                return amount / coins[ind];
-            } else {
-                return (int)1e9;
-            }
-        }
-        if (dp[ind][amount] != -1) return dp[ind][amount];
-
-        int notPick = coinChange(ind - 1, amount, coins, dp);
-        int pick = Integer.MAX_VALUE;
-        if (amount >= coins[ind]) {
-            pick = 1 + coinChange(ind, amount - coins[ind], coins, dp);
-        }
-
-        return dp[ind][amount] = Math.min(pick, notPick);
-        
-    }
-
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         int[][] dp = new int[n][amount + 1];
         
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
+        for (int tar = 0; tar <= amount; tar++) {
+            if (tar % coins[0] == 0) {
+                dp[0][tar] = tar / coins[0];
+            } else {
+                dp[0][tar] = (int)1e9;
+            }
         }
 
-        int ans = coinChange(n - 1, amount, coins, dp);
-        if (ans >= 1e9) {
-            return -1;
+        for (int i = 1; i < n; i++) {
+            for (int tar = 0; tar <= amount; tar++) {
+                int notPick = dp[i - 1][tar];
+                int pick = Integer.MAX_VALUE;
+                if (tar >= coins[i]) {
+                    pick = 1 + dp[i][tar - coins[i]];
+                }
+                dp[i][tar] = Math.min(pick, notPick);
+            }
         }
-        return ans;
+
+        return dp[n-1][amount] >= 1e9 ? -1 : dp[n-1][amount];
     }
 }

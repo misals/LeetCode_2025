d12dbea52876bcd89f7559c8e42813d1df265ff8
Suraj Misal <35091245+misals@users.noreply.github.com>
2025-08-10 07:59:28 +0530

    Time: 0 ms (100%), Space: 41 MB (47.37%) - LeetHub

---
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.java
@@ -0,0 +1,24 @@
+class Solution {
+    private int freq[] = new int[10];
+    public boolean reorderedPowerOf2(int n) {
+        while (n > 0){
+            freq[n % 10]++;
+            n /= 10;
+        }
+        for (int i = 0; i < 31; i++)
+            if (check(1 << i))
+                return true;
+        return false;
+    }
+    private boolean check(int n){
+        int count[] = new int[10];
+        while (n > 0){
+            count[n % 10]++;
+            n /= 10;
+        }
+        for (int i = 0; i < 10; i++)
+            if (freq[i] != count[i])
+                return false;
+        return true;
+    }
+}

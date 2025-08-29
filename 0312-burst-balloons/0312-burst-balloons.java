290d7f7c163a50f95b2e1f852655826bac71b469
Suraj Misal <35091245+misals@users.noreply.github.com>
2025-08-29 20:02:28 +0530

    Time: 31 ms (97.88%), Space: 42.9 MB (30.07%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,17 +1,4 @@
 class Solution {
-    public int maxCoins(int i, int j, int[] nums, int[][] dp) {
-        if (i > j) return 0;
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int maxi = 0;
-        for (int k = i; k <= j; k++) {
-            int val = nums[i - 1] * nums[k] * nums[j + 1] + maxCoins(i, k - 1, nums, dp) + maxCoins(k + 1, j, nums, dp);
-            maxi = Math.max(maxi, val);
-        }
-        return dp[i][j] = maxi;
-    }
-
     public int maxCoins(int[] arr) {
         int n = arr.length;
 
@@ -25,10 +12,19 @@ class Solution {
 
         int[][] dp = new int[n + 2][n + 2];
 
-        for (int i = 0; i < n + 2; i++) {
-            Arrays.fill(dp[i], -1);
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n; j++) {
+                if (i > j) continue;
+
+                int maxi = 0;
+                for (int k = i; k <= j; k++) {
+                    int val = nums[i - 1] * nums[k] * nums[j + 1] + dp[i][k - 1] + dp[k + 1][j];
+                    maxi = Math.max(maxi, val);
+                }
+                dp[i][j] = maxi;
+            }
         }
 
-        return maxCoins(1, n, nums, dp);
+        return dp[1][n];
     }
 }

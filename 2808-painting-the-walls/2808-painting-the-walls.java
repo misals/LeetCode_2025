842866dfb65545f660cf2b8d3cdd785a86a2af77
Suraj Misal <35091245+misals@users.noreply.github.com>
2025-10-03 12:31:14 +0530

    Time: 40 ms (51.8%), Space: 45.9 MB (41.31%) - LeetHub

--- 2808-painting-the-walls/2808-painting-the-walls.java
+++ 2808-painting-the-walls/2808-painting-the-walls.java
@@ -3,20 +3,23 @@ class Solution {
         if (remaining <= 0) {
             return 0;
         }
-        if (ind < 0) return 2000000000;
+        if (ind < 0) {
+            return (int)1e9;
+        }
+        if (dp[ind][remaining] != -1) {
+            return dp[ind][remaining];
+        }
 
-        if (dp[ind][remaining] != -1) return dp[ind][remaining];
+        int notPick = paintWalls(ind - 1, remaining, cost, time, dp);
+        int pick = cost[ind] + paintWalls(ind - 1, remaining - 1 - time[ind], cost, time, dp);
 
-        int notPaint = paintWalls(ind - 1, remaining, cost, time, dp);
-        int paint = cost[ind] + paintWalls(ind - 1, remaining - 1 - time[ind], cost, time, dp);
-
-        return dp[ind][remaining] = Math.min(notPaint, paint);
+        return dp[ind][remaining] = Math.min(pick, notPick);
     }
 
     public int paintWalls(int[] cost, int[] time) {
         int n = cost.length;
 
-        int[][] dp = new int[n][n+1];
+        int[][] dp = new int[n][n + 1];
 
         for (int i = 0; i < n; i++) {
             Arrays.fill(dp[i], -1);

e72688798d46c2613de74dcb1eeb5976034a5f1f
Suraj Misal <35091245+misals@users.noreply.github.com>
2025-10-04 15:33:33 +0530

    Time: 115 ms (20.74%), Space: 42.5 MB (91.69%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,30 +1,34 @@
 class Solution {
-    public int maxCoins(int[] arr) {
-        int n = arr.length;
+    public int maxCoins(int i, int j, int[] arr, int[][] dp) {
+        if (i > j) return 0;
 
-        int[] nums = new int[n + 2];
-        nums[0] = 1;
-        nums[n + 1] = 1;
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int ans = Integer.MIN_VALUE;
+
+        for (int k = i; k <= j; k++) {
+            int val = arr[i - 1] * arr[k] * arr[j + 1] + maxCoins(i, k - 1, arr, dp) + maxCoins(k + 1, j, arr, dp);
+            ans = Math.max(ans, val);
+        }
+        return dp[i][j] = ans;
+    }
+
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+
+        int[] arr = new int[n + 2];
+        arr[0] = 1;
+        arr[n + 1] = 1;
 
         for (int i = 0; i < n; i++) {
-            nums[i + 1] = arr[i];
+            arr[i + 1] = nums[i];
         }
 
         int[][] dp = new int[n + 2][n + 2];
 
-        for (int i = n; i >= 1; i--) {
-            for (int j = 1; j <= n; j++) {
-                if (i > j) continue;
-
-                int maxi = 0;
-                for (int k = i; k <= j; k++) {
-                    int val = nums[i - 1] * nums[k] * nums[j + 1] + dp[i][k - 1] + dp[k + 1][j];
-                    maxi = Math.max(maxi, val);
-                }
-                dp[i][j] = maxi;
-            }
+        for (int i = 0; i < n + 2; i++) {
+            Arrays.fill(dp[i], -1);
         }
-
-        return dp[1][n];
+        return maxCoins(1, n, arr, dp);
     }
 }

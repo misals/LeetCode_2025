703c0e7122f27d3dbf126fc40ed394be6ac07ab4
Suraj Misal <35091245+misals@users.noreply.github.com>
2026-04-18 14:57:27 +0530

    Time: 2 ms (99.6%), Space: 44.8 MB (89.95%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,27 +1,28 @@
 class Solution {
-    public void findSubset(int ind, int n, int[] nums, List<Integer> temp, Set<List<Integer>> ans) {
-        if (ind == n) {
-            ans.add(new ArrayList<>(temp));
-            return;
-        }
-        for (int i = ind; i < n; i++) {
-            if (i > ind && nums[i] == nums[i-1]) continue;
+    public void subsetsWithDup(int ind, int n, int[] nums, List<Integer> temp, List<List<Integer>> ans) {
+      
+        ans.add(new ArrayList<>(temp));
 
+        for (int i = ind; i < n; i++) {
+            if (i > ind && nums[i] == nums[i - 1]) {
+                continue;
+            }
             temp.add(nums[i]);
-            findSubset(i+1, n, nums, temp, ans);
+            subsetsWithDup(i + 1, n, nums, temp, ans);
             temp.remove(temp.size() - 1);
         }
-        findSubset(ind+1, n, nums, temp, ans);
     }
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
         int n = nums.length;
-        Set<List<Integer>> ans = new HashSet<>();
+        Arrays.sort(nums);
 
-        findSubset(0, n, nums, new ArrayList<>(), ans);
-        List<List<Integer>> res = new ArrayList<>(ans);
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
 
-        return res;
+        subsetsWithDup(0, n, nums, temp, ans);
+
+        return ans;
+
     }
 }

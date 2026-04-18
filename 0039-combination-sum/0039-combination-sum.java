e5c7d33cc96771c9b1ff5d660b014d8e132d5d99
Suraj Misal <35091245+misals@users.noreply.github.com>
2026-04-18 13:26:56 +0530

    Time: 3 ms (33.9%), Space: 45.5 MB (92.4%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,33 +1,29 @@
 class Solution {
-    public void combinationSum(int ind, int[] candidates, int target, List<Integer> list, Set<List<Integer>> ans) {
-        if (target == 0) {
-            ans.add(new ArrayList<>(list));
-        }
-        if (ind < 0) {
+    public void combinationSum(int ind, int n, int target, int[] candidates, List<Integer> temp, Set<List<Integer>> ans) {
+        if (ind == n) {
+            if (target == 0) {
+                ans.add(new ArrayList<>(temp));
+            }
             return;
         }
 
-        combinationSum(ind - 1, candidates, target, list, ans);
+        combinationSum(ind + 1, n, target, candidates, temp, ans);
+
         if (target >= candidates[ind]) {
-            list.add(candidates[ind]);
-            combinationSum(ind, candidates, target - candidates[ind], list, ans);
-            list.remove(list.size() - 1);
-        } 
+            temp.add(candidates[ind]);
+            combinationSum(ind, n, target - candidates[ind], candidates, temp, ans);
+            temp.remove(temp.size() - 1);
+        }
     }
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         int n = candidates.length;
 
-        int[][] dp = new int[n][target + 1];
-
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-
         Set<List<Integer>> ans = new HashSet<>();
-        List<Integer> list = new ArrayList<>();
 
-        combinationSum(n - 1, candidates, target, list, ans);
+        List<Integer> temp = new ArrayList<>();
+
+        combinationSum(0, n, target, candidates, temp, ans);
 
         List<List<Integer>> res = new ArrayList<>(ans);
 

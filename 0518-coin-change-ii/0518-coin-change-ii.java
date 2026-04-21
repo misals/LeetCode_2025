a5dc6b4485f50a5d94daebf16ba7d227e91936a7
Suraj Misal <35091245+misals@users.noreply.github.com>
2026-04-21 09:32:07 +0530

    Time: 16 ms (34.41%), Space: 57.3 MB (22.12%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,24 +1,25 @@
 class Solution {
-    public int coinChange(int ind, int amount, int[] coins, int[][] dp) {
+    public int coinChange(int i, int amount, int[] coins, int[][] dp) {
         if (amount == 0) {
             return 1;
         }
-        if (ind == 0) {
-            if (amount % coins[0] == 0) {
+        if (i == 0) {
+            if (amount % coins[i] == 0) {
                 return 1;
-            } else {
-                return 0;
             }
+            return 0;
         }
-        if (dp[ind][amount] != -1) return dp[ind][amount];
 
-        int notTake = coinChange(ind - 1, amount, coins, dp);
+        if (dp[i][amount] != -1) {
+            return dp[i][amount];
+        }
+        int notTake = coinChange(i - 1, amount, coins, dp);
         int take = 0;
-        if (amount >= coins[ind]) {
-            take = coinChange(ind, amount - coins[ind], coins, dp);
+        if (amount >= coins[i]) {
+            take = coinChange(i, amount - coins[i], coins, dp);
         }
 
-        return dp[ind][amount] = take + notTake;
+        return dp[i][amount] = take + notTake;
     }
 
     public int change(int amount, int[] coins) {

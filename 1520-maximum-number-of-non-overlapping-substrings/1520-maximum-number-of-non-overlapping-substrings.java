9c0f14a2b454ca7803980e6840a047cff7b6790e
Suraj Misal <35091245+misals@users.noreply.github.com>
2026-09-18 09:05:53 +0530

    Time: 8 ms (96.16%), Space: 47.9 MB (88.24%) - LeetHub

---
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.java
@@ -0,0 +1,67 @@
+class Solution {
+    public List<String> maxNumOfSubstrings(String s) {
+        int n = s.length();
+
+        int[] first = new int[26];
+        int[] last = new int[26];
+
+        Arrays.fill(first, n);
+        Arrays.fill(last, -1);
+
+        for (int i = 0; i < n; i++) {
+            int c = s.charAt(i) - 'a';
+
+            first[c] = Math.min(first[c], i);
+            last[c] = i;
+        }
+
+        List<int[]> intervals = new ArrayList<>();
+
+        for (int c = 0; c < 26; c++) {
+            if (last[c] == -1) {
+                continue;
+            }
+
+            int start = first[c];
+            int end = last[c];
+
+            boolean valid = true;
+
+            for (int i = start; i <= end; i++) {
+                int x = s.charAt(i) - 'a';
+
+                if (first[x] < start) {
+                    valid = false;
+                    break;
+                }
+
+                end = Math.max(end, last[x]);
+            }
+            if (valid) {
+                intervals.add(new int[]{start, end});
+            }
+        }
+
+        intervals.sort((a, b) -> {
+            if (a[1] != b[1]) {
+                return Integer.compare(a[1], b[1]);
+            }
+            return Integer.compare(a[1] - a[0], b[1] - b[0]);
+        });
+
+        List<String> answer = new ArrayList<>();
+        int previousEnd = -1;
+
+        for (int[] interval : intervals) {
+            int start = interval[0];
+            int end = interval[1];
+
+            if (start > previousEnd) {
+                answer.add(s.substring(start, end + 1));
+                previousEnd = end;
+            }
+        }
+
+        return answer;
+    }
+}
